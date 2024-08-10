02f065c544a24fb638d2597f52f4ac1f5c453764
Łukasz Gendosz <genda1231@gmail.com>
2024-08-10 22:39:45 +0200

    Add config

---
+++ config.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn, field_validator
+from typing import Any, Optional, Union
+from pydantic_core.core_schema import FieldValidationInfo
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=Path(__file__).parent.parent/".env",
+                                      env_file_encoding='utf-8',
+                                      extra='allow')
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
+    
+    
+    def generate_db_uri(self) -> None:
+        self.DATABASE_URI = PostgresDsn.build(
+            scheme= 'postgresql+asyncpg',
+            username = self.POSTGRES_USER,
+            password = self.POSTGRES_PASSWORD,
+            host = self.POSTGRES_SERVER,
+            port= self.POSTGRES_PORT,
+            path = self.POSTGRES_DB
+        ).unicode_string()
+            
+    
+config = Settings(_env_file=Path(__file__).parent.parent/".env")
+config.generate_db_uri()
