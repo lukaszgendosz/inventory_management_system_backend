95189d6abc4f11096d5af4d112f31721912158b9
Łukasz Gendosz <genda1231@gmail.com>
2024-08-26 23:09:31 +0200

    --allow-empty-message

--- main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from routers import users, assets
-
-app = FastAPI(title="Inventory Management System")
-
-app.include_router(users.router)
-app.include_router(assets.router)
--- routers/users.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(
-    prefix='/api/v1/users'
-)
