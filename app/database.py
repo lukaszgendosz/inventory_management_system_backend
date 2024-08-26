a735ae32b6f6be34e3761a63fded7da00a84a9fc
Łukasz Gendosz <genda1231@gmail.com>
2024-08-26 23:04:33 +0200

    update database

---
+++ app/database.py
@@ -0,0 +1,27 @@
+from typing import Callable
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session, scoped_session
+from contextlib import AbstractContextManager, contextmanager
+
+class Database:
+
+    def __init__(self, db_url: str) -> None:
+        self._engine = create_engine(db_url, echo=True)
+        self._session_factory = scoped_session(
+            sessionmaker(
+                autocommit=False,
+                autoflush=False,
+                bind=self._engine,
+            ),
+        )
+
+    @contextmanager
+    def session(self) -> Callable[..., AbstractContextManager[Session]]:
+        session: Session = self._session_factory()
+        try:
+            yield session
+        except Exception:
+            session.rollback()
+            raise
+        finally:
+            session.close()
