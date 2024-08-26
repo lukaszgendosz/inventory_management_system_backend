f3df8d57bbbc8336f836d49ef80b9b4d8ed4a5ed
Łukasz Gendosz <genda1231@gmail.com>
2024-08-26 23:02:18 +0200

    add user repositorty

---
+++ app/repositories/user_repository.py
@@ -0,0 +1,15 @@
+from typing import Iterator
+
+from app.repositories.base_repository import BaseRepository
+from app.models.user import User
+
+class UserRepository(BaseRepository):
+    
+    def get_all(self) -> Iterator[User]:
+        with self.session_factory() as session:
+            return session.query(User).all()
+        
+    def get_by_id(self, user_id: int) -> User:
+        with self.session_factory() as session:
+            user = session.query(User).filter(User.id == user_id).first()
+            return user
