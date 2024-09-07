c47b61c014a2aeaa5250d7ca250a1aac8d6e7e82
Łukasz Gendosz <genda1231@gmail.com>
2024-09-07 18:02:53 +0200

    Add roles

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,14 @@
-from app.models.base import Base
-from sqlalchemy import Column, Integer, String, Text, Boolean, TIMESTAMP, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
 
+from sqlalchemy import Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.schemes.role_enum import Role
+from .base import Base
+
+if TYPE_CHECKING:
+    from .department import Department
+    
 class User(Base):
     __tablename__ = 'users'
     
@@ -9,12 +16,13 @@ class User(Base):
     username: Mapped[str] = mapped_column(nullable=False)
     first_name: Mapped[str] = mapped_column(nullable=False)
     last_name: Mapped[str] = mapped_column(nullable=False)
+    role: Mapped[Role] = mapped_column(nullable=False, default=Role.USER)
     password: Mapped[str] = mapped_column(nullable=False)
     notes: Mapped[str] = mapped_column(Text, nullable=True)
-    location_id: Mapped[int] = mapped_column(nullable=True)
-    company_id: Mapped[int] = mapped_column(nullable=True)
-    department_id: Mapped[int] = mapped_column(nullable=True)
-    is_active: Mapped[bool] = mapped_column(nullable=True, default=True)
+    location_id: Mapped[int] = mapped_column(nullable=True, default=None)
+    company_id: Mapped[int] = mapped_column(nullable=True, default=None)
+    is_active: Mapped[bool] = mapped_column(nullable=False, default=True)
+    
 
 
 
