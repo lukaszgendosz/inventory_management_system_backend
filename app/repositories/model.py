7736b404d2e9956c8dd31a562763d35c66e1edd2
Łukasz Gendosz <genda1231@gmail.com>
2024-11-26 10:44:24 +0100

    Add custom filters

--- app/repositories/model.py
+++ app/repositories/model.py
@@ -1,6 +1,13 @@
+from typing import Any, List
 from app.repositories.base import BaseRepository
 from app.models import Model
+from app.schemes import ModelParamsScheme
 
 
 class ModelRepository(BaseRepository[Model]):
-    pass
+
+    def _generate_filters(self, params: ModelParamsScheme) -> List[Any]:
+        filters = super()._generate_filters(params)
+        if params.manufacturer_id:
+            filters.append(self.model_class.manufacturer_id.in_(params.manufacturer_id))
+        return filters
