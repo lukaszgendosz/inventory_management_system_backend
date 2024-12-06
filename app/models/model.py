467053a50098a7b09df2369d11a2b603e66998d0
Łukasz Gendosz <genda1231@gmail.com>
2024-12-06 13:51:59 +0100

    Remove category relation

--- app/models/model.py
+++ app/models/model.py
@@ -7,7 +7,6 @@ from .base import Base
 
 if TYPE_CHECKING:
     from .manufacturer import Manufacturer
-    from .category import Category
     from .asset import Asset
 
 
@@ -18,10 +17,11 @@ class Model(Base):
     model_number: Mapped[str] = mapped_column(nullable=True, unique=True)
     notes: Mapped[str] = mapped_column(Text, nullable=True)
 
-    manufacturer_id: Mapped[int] = mapped_column(ForeignKey("manufacturers.id"), nullable=True, default=None)
-    manufacturer: Mapped["Manufacturer"] = relationship(back_populates="models", lazy="joined")
-
-    # category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"), nullable=True, default=None)
-    # category: Mapped["Category"] = relationship(back_populates="models", lazy="joined")
+    manufacturer_id: Mapped[int] = mapped_column(
+        ForeignKey("manufacturers.id"), nullable=True, default=None
+    )
+    manufacturer: Mapped["Manufacturer"] = relationship(
+        back_populates="models", lazy="joined", overlaps="manufacturer"
+    )
 
     assets: Mapped[list["Asset"]] = relationship("Asset", backref="models")
