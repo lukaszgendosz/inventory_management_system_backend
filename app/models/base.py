6af0dd5db3ba9bf2a8ab1befd69d7e87d504ac64
Łukasz Gendosz <genda1231@gmail.com>
2024-12-06 13:52:16 +0100

    Change formatting

--- app/models/base.py
+++ app/models/base.py
@@ -5,7 +5,9 @@ from sqlalchemy import func, DateTime
 
 
 class Base(DeclarativeBase):
-    id: Mapped[int] = mapped_column(primary_key=True, nullable=False, unique=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(
+        primary_key=True, nullable=False, unique=True, autoincrement=True
+    )
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
